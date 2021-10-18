377ccbe54a0a2df489427f9f127cb026b3325c34
Vinay Reddy <89741616+VinayReddy-K@users.noreply.github.com>
2021-10-18 20:13:31 +0530

    config security group to ec2

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "sample" {
-  ami           = ami-0e4e4b2f188e91845
-  instance_type = "t2.micro"
+  ami                    = ami-0e4e4b2f188e91845
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  tags          = {
-    Name        = "sample"
- }
+  tags                   = {
+    Name = "sample"
+  }
+}
 
 
 resource "aws_security_group" "allow_ssh" {
