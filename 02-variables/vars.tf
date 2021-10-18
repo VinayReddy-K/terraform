4cea19a82b9b8d051d0f0162e7aa372419d029c8
Vinay Reddy <89741616+VinayReddy-K@users.noreply.github.com>
2021-10-18 17:22:47 +0530

    passing variables

--- 02-variables/vars.tf
+++ 02-variables/vars.tf
@@ -75,23 +75,23 @@ output "TRAININGS" {
 
 // Variables from tfvars
 
-//variable "FRUITS" {}
-//
-//output "FRUITS" {
-//  value = var.FRUITS
-//}
-//
-//variable "COUNTRY" {}
-//output "COUNTRY" {
-//  value = var.COUNTRY
-//}
-//
-//variable "SEASON" {}
-//output "SEASON" {
-//  value = var.SEASON
-//}
-//
-//variable "FRUIT" {}
-//output "FRUIT" {
-//  value = var.FRUIT
-//}
+variable "FRUITS" {}
+
+output "FRUITS" {
+  value = var.FRUITS
+}
+
+variable "COUNTRY" {}
+output "COUNTRY" {
+  value = var.COUNTRY
+}
+
+variable "SEASON" {}
+output "SEASON" {
+  value = var.SEASON
+}
+
+variable "FRUIT" {}
+output "FRUIT" {
+  value = var.FRUIT
+}
