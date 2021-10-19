47552c1fc7c1b108db84a1bba2d7a280ed1d5314
Vinay Reddy <89741616+VinayReddy-K@users.noreply.github.com>
2021-10-19 11:14:41 +0530

    count

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "sample" {
+  count                  = 2
   ami                    = "ami-0e4e4b2f188e91845"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.SG_ID]
 
   tags                   = {
-    Name = "sample"
+    Name = "sample-${count.index+1}"
   }
 }
 
